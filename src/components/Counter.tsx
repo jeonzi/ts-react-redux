5b51ab6231a3008661e7081dc52579ff2cf23862
jieun jeon <jieun5230@naver.com>
2020-07-03 17:53:50 +0900

    todoList redux type setting and make reducer

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,30 @@
 import React from "react"
+import useCounter from "../hooks/useCounter"
 
-type CounterProps = {
-  count: number
-  onIncrease: () => void
-  onDecrease: () => void
-  onIncreaseBy: (diff: number) => void
-}
+/** Presentational Component */
+// Presentational과 Container를 분리해서 사용하는 경우
+// type CounterProps = {
+//   count: number
+//   onIncrease: () => void
+//   onDecrease: () => void
+//   onIncreaseBy: (diff: number) => void
+// }
+//
+// function Counter({ count, onIncrease, onDecrease, onIncreaseBy }: CounterProps) {
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={onIncrease}>+1</button>
+//       <button onClick={onDecrease}>-1</button>
+//       <button onClick={() => onIncreaseBy(5)}>+5</button>
+//     </div>
+//   )
+// }
 
-function Counter({ count, onIncrease, onDecrease, onIncreaseBy }: CounterProps) {
+// Presentational 구분 하지 않을 때, useCounter의 Hook을 사용한다
+function Counter() {
+  const { count, onIncrease, onDecrease, onIncreaseBy } = useCounter()
+
   return (
     <div>
       <h1>{count}</h1>
